8eea85f5c21091de5a9f5072f65df0cfa3aa2b35
Bruno Quindeler <bruno.quindeler@gmail.com>
2023-07-30 17:54:47 -0300

    add arrays

---
+++ arrays/indexes.js
@@ -0,0 +1,9 @@
+// This is an array of strings
+var fruits = ["apple", "banana", "pineapple", "strawberry"];
+
+// We set the variable banana to the value of the second element of
+// the fruits array. remember that indices start ar 0, so 1 is the
+// second element. Result: banana = "banana"
+var banana = fruits[1];
+
+console.log(banana); // banana
---
+++ arrays/length.js
@@ -0,0 +1,6 @@
+var array = [1, 2, 3];
+
+// Result: l = 3
+var l = array.length;
+
+console.log(l); // 3
